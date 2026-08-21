04a2932d56ad75c9bddbf276a0f7ce2a9bf1959d
Vishal Chaudhary <vishalchaudhary969518@gmail.com>
2026-08-21 23:04:22 +0530

    solve LC42_Trapping_Rain_Water problem

---
+++ Arrays/LC42_Trapping_Rain_Water.java
@@ -0,0 +1,43 @@
+/**
+ * LC42_Trapping_Rain_Water
+ */
+public class LC42_Trapping_Rain_Water {
+
+   
+    public int trap(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left <= right) {
+
+            if (height[left] <= height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
