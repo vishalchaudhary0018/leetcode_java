d8876b6ee2c7aea11ae0645a66ef7ecf8ca3ee58
Vishal Chaudhary <vishalchaudhary969518@gmail.com>
2026-08-22 23:27:10 +0530

    this is tha basic od string in java v1

---
+++ Strings/BasicOfString.java
@@ -0,0 +1,12 @@
+/**
+ * BasicOfString
+ */
+public class BasicOfString {
+
+   public static void main(String[] args) {
+      String s1= "vishal ";
+      String s2="chaudhary ";
+      String s3=new String("java developer");
+      System.out.println(s1+s2+s3);
+   }
+}
