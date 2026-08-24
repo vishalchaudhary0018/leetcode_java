1c7818dc1e27d93602204109ca03c6ef8b3727cf
Vishal Chaudhary <vishalchaudhary969518@gmail.com>
2026-08-24 22:43:00 +0530

    update

--- Strings/BasicOfString.java
+++ Strings/BasicOfString.java
@@ -4,9 +4,12 @@
 public class BasicOfString {
 
    public static void main(String[] args) {
-      String s1= "vishal ";
-      String s2="chaudhary ";
-      String s3=new String("java developer");
-      System.out.println(s1+s2+s3);
+      String s1 = "vishal ";
+      String s2 = "chaudhary ";
+      String s3 = new String("java developer");
+      System.out.println(s1);
+      System.out.println(s2);
+      System.out.println(s3);
+
    }
 }
